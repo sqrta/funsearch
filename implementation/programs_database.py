a95c76ad0c35dc321fb8be176ed2fc6d2b39aeac
sqrta <jackdhw@mail.ustc.edu.cn>
2025-01-19 21:34:41 -0500

    update ramdom choice

--- implementation/programs_database.py
+++ implementation/programs_database.py
@@ -233,7 +233,7 @@ class Island:
         functions_per_prompt = min(len(self._clusters), self._functions_per_prompt)
 
         idx = np.random.choice(
-            len(signatures), size=functions_per_prompt, p=probabilities
+            len(signatures), size=functions_per_prompt, p=probabilities, replace=False
         )
         chosen_signatures = [signatures[i] for i in idx]
         implementations = []
